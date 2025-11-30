2ca47efa0db3f5ffa121f036b4cb1c0bcf224500
Neteno <szymon.andrzejczak@spoko.pl>
2025-11-30 19:14:46 +0100

    Edit Wygląd w index Entry

--- DTOs/ExerciseEntryDTO.cs
+++ DTOs/ExerciseEntryDTO.cs
@@ -7,7 +7,9 @@ namespace BeFitBlazor.DTOs
     {
         public int Id { get; set; }
         public int TrainingSessionId { get; set; }
+        public string TrainingSessionName { get; set; }
         public int ExerciseTypeId { get; set; }
+        public string ExerciseTypeName { get; set; }
         public int Weight { get; set; }
         public int Sets { get; set; }
         public int Reps { get; set; }
@@ -16,7 +18,9 @@ namespace BeFitBlazor.DTOs
         {
             Id = exerciseEntry.Id;
             TrainingSessionId = exerciseEntry.TrainingSessionId;
+            TrainingSessionName = exerciseEntry.TrainingSession?.Start.ToString() ?? "";
             ExerciseTypeId = exerciseEntry.ExerciseTypeId;
+            ExerciseTypeName = exerciseEntry.ExerciseType?.Name ?? "";
             Weight = exerciseEntry.Weight;
             Sets = exerciseEntry.Sets;
             Reps = exerciseEntry.Reps;
